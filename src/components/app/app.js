c4d0bed163736dc6e856c1563e8b1ff7d24ff503
AllNNBusy <Butek63rus@gmail.com>
2023-02-15 04:17:15 +0300

    interface-template

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeeList from '../employees-list/employees-list';
+import EmployeeAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeeList />
+      <EmployeeAddForm />
+    </div>
+  );
+}
+
+export default App;
